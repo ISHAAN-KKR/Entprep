f9d538615c07410601cb14e701d405f7e7dbf061
Uhashini <uhashininithilan@gmail.com>
2025-06-19 21:11:09 +0530

    Set light mode as default theme

--- components/theme-provider.jsx
+++ components/theme-provider.jsx
@@ -6,7 +6,7 @@ export function ThemeProvider({ children }) {
   return (
     <NextThemesProvider
       attribute="class"
-      defaultTheme="system"
+      defaultTheme="light"
       enableSystem
       disableTransitionOnChange
     >
